dc98c3a46a5c7d13dde9b88ada5b39c72fdd8c04
ViktoriiaTan <viktantsiura@gamil.com>
2024-01-22 16:24:54 +0000

    Try different optimizer

--- script/train.py
+++ script/train.py
@@ -5,7 +5,7 @@ Module for training the neural network model.
 
 def train_model(model, x_train, y_train, epochs=15, batch_size=128, callbacks=None):
     model.compile(loss="categorical_crossentropy",
-                  optimizer="adam",
+                  optimizer="sgd",
                   metrics=["accuracy"])
     history = model.fit(x_train, y_train,
               batch_size=batch_size,
