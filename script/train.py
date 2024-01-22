834b0f9b93351b33541b99c82e95b292f26a1480
ViktoriiaTan <viktantsiura@gamil.com>
2024-01-22 17:18:05 +0000

    Try different learning rate

--- script/train.py
+++ script/train.py
@@ -1,11 +1,12 @@
 """
 Module for training the neural network model.
 """
+from tensorflow.keras.optimizers import Adam
 
 
 def train_model(model, x_train, y_train, epochs=15, batch_size=128, callbacks=None):
     model.compile(loss="categorical_crossentropy",
-                  optimizer="sgd",
+                  optimizer=Adam(learning_rate=0.01),
                   metrics=["accuracy"])
     history = model.fit(x_train, y_train,
               batch_size=batch_size,
