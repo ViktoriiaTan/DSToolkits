509f5c43657e96b9fe51e82e09605a34e39da034
ViktoriiaTan <viktantsiura@gamil.com>
2024-01-22 16:02:14 +0000

    Changed batches, renoming unnecessary lines of code

--- script/1_main.py
+++ script/1_main.py
@@ -3,9 +3,7 @@ The core module for organizing data loading, model creation,
 training, and evaluation.
 """
 import numpy as np
-import pickle
 import wandb
-import os
 from wandb.keras import WandbCallback
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
@@ -18,7 +16,7 @@ from eval_predict import eval_model, predict
 
 def main():
     # Initialize Weights & Biases
-    wandb.init(project="mnist_digit_classification", entity="tantsuraviktoria")
+    wandb.init(project="mnist_digit_classification", entity="tantsuraviktoria", config={"architecture": "Lessbatches"})
     
     model_file = '../data/mnist_model.h5'
     
@@ -37,17 +35,14 @@ def main():
     print("Creating model...")
     model = create_model(input_shape, num_classes)
 
-    # Compile the model with accuracy as a metric
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
     # Train the model
     print("Training model...")
     
     # Train the model and log the metric
-    history = model.fit(
+    history = train_model(model,
     x_train, y_train,
-    validation_data=(x_test, y_test),
     epochs=10,
+    batch_size=64,
     callbacks=[WandbCallback()]
     )
 
@@ -68,7 +63,7 @@ def main():
     # Make predictions
     y_pred = model.predict(x_test)
     
-    # Convert predictions to label indices if they're in a one-hot encoded format
+    # Convert predictions to label indices
     preds = np.argmax(y_pred, axis=1)
     y_true = np.argmax(y_test, axis=1)
     
